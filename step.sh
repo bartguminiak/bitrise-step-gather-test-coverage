03f3d88b0b6c96e081ebd69220ce9772e8d41bc8
Bartłomiej Guminiak <guminiak.bartlomiej@gmail.com>
2019-09-16 21:05:22 +0200

    test

--- step.sh
+++ step.sh
@@ -3,6 +3,8 @@ set -ex
 
 gem install slather -v 2.4.7
 
+slather coverage -s --ignore "Pods/*" --scheme DrugStars --workspace "$workspace_path" "$project_path"
+
 output=$(slather coverage -s --ignore "Pods/*" --scheme DrugStars --workspace "$workspace_path" "$project_path")
 output=${output##*Coverage: }
 output=${output%%Slathered*}
